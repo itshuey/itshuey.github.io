38e72eca8d1c8cf360b61a6689e7615b591d16d9
Huey Sun <hubert.s.sun@gmail.com>
2019-08-03 16:32:54 -0700

    Setting up basic project titles

--- src/pages/Projects.js.jsx
+++ src/pages/Projects.js.jsx
@@ -8,33 +8,66 @@ class Projects extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      extend: false
+      showTitle: true,
+      extend: false,
     };
   }
 
   render() {
-    return(
-      <div>
-        <div className={"content"}>
-          <div className={"header"}>
-            <b><u>VER II</u></b>
-          </div>
-          <div className={"overlay red neon"}>
-            projects
-          </div>
-          <div className={"floating"}>
-            <br /><br /><b><i>A SELECTED SERIES
-            <br />A SELECTED SERIES
-            <br />A SELECTED SERIES</i></b>
-          </div>
-          <div className={"placeholder"} />
-          <button className="reveal red">
-            <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SOON!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b>
-          </button>
+    let title = (
+      <div className={"content"}>
+        <div className={"header"}>
+          <b><u>VER II</u></b>
+        </div>
+        <div className={"overlay red neon"}>
+          projects
+        </div>
+        <div className={"floating"}>
+          <br /><br /><b><i>A SELECTED SERIES
+          <br />A SELECTED SERIES
+          <br />A SELECTED SERIES</i></b>
+        </div>
+        <div className={"placeholder"} />
+        <button className={"reveal red"} onClick={() => this.setState({ showTitle: false })}>
+          <b>!!!</b>
+        </button>
+      </div>
+    );
+
+    return this.state.showTitle ? title : this.renderContents();
+  }
+
+  renderProjectTitle(title, tags) {
+    return (
+      <div className="projectTitle">
+        <div className="projectHeader"> {title} </div>
+        <div className="projectTags"> {tags} </div>
+        <button className="projectDemo">
+            DEMO / INFO
+        </button>
+      </div>
+    );
+  }
+
+  renderContents() {
+    return (
+      <div className={"content"}>
+        <div className={"projectsGrid"}>
+          {this.renderProjectTitle("gradeBook", "Java, MySQL, AWS, NetBeans, Swing")}
+          {this.renderProjectTitle("Netflix+", "Javascript, Browserify, Node.js")}
+          {this.renderProjectTitle("tweet-me", "Python, Beautiful Soup, etc.")}
+          {this.renderProjectTitle("hueysun.com", "Javascript, React.js, HTML, CSS")}
+        </div>
+        <div className={"projectsButton back"} onClick={() => this.setState({ showTitle: true })}>
+          <b>BACK</b>
+        </div>
+        <div className={"projectsButton more"} onClick={() => this.setState({ showTitle: true })}>
+          <b>MORE</b>
         </div>
       </div>
     );
   }
+
 }
 
 export default Projects;
