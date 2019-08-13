eb751eb358bf8d182ff1b68a442967bbaf95c07f
Huey Sun <hubert.s.sun@gmail.com>
2019-08-13 15:14:46 -0700

    First draft: expaning projects tab

--- src/pages/Projects.js.jsx
+++ src/pages/Projects.js.jsx
@@ -9,7 +9,11 @@ class Projects extends React.Component{
     super(props);
     this.state = {
       showTitle: true,
-      currentProject: ""
+      currentProject: "",
+      netflix_tab: "desc",
+      gradebook_tab: "desc",
+      tweetme_tab: "desc",
+      website_tab: "desc",
     };
   }
 
@@ -52,42 +56,71 @@ class Projects extends React.Component{
     );
   }
 
+  renderProjectTags(current, source) {
+    return (
+      <div className="tags">
+        <a className="tag">description</a>
+        <a className="tag">demo</a>
+        <a className="tag">technical</a>
+        <a className="tag">source</a>
+      </div>
+    );
+  }
+
   renderProject(render, tags) {
     let title = this.state.currentProject;
-    let description, demo;
+    let description, demo, technical;
+    let contentState, content;
 
     if (title == "Netflix+"){
-      description = "A chrome extension that adds an expiry indicator into browsing view!";
+      contentState = this.state.netfix_tab;
+      description = "A chrome extension that modifies netflix's website to include \
+                    indicators for expiring content that show as you browse!";
 
     } else if (title == "gradeBook") {
+      contentState = this.state.gradebook_tab;
+
       description = "A java application that stores input assignments to calculate your grades! \
-                    Features local storage via xml or cloud storage via AWS RDS MySql.";
+                    Features local XML storage or cloud storage via a AWS MySQL database.";
 
     } else if (title == "tweet-me") {
+      contentState = this.state.tweetme_tab;
       description = "Tweet-me takes in an input twitter handle or local .txt file and \
                     uses a markov-chain algorithm to generate a tweet in that style!";
 
     } else if (title == "hueysun.com") {
+      contentState = this.state.website_tab;
       description = "Hope you've been enjoying my website! It's built in react.js, \
                     and is version two of my yearly effort to rebuild my site with \
                     the things I leared in the previous year.";
 
     };
 
+    if (contentState = "desc") {
+      content = description;
+    } else if (contentState = "demo") {
+      content = demo;
+    } else if (contentState = "technical") {
+      content = technical;
+    }
+
     return (
       <div className="content">
-        <div className="project-title">
-          {title}
-        </div>
-        <div className="project-body">
-          <div className="project-description">{description}</div>
-          <div className="project-demo">{demo}</div>
+        <div className="project-content">
+          <div className="project-title">
+            {title}
+          </div>
+          <div className="project-body">
+            {content}
+          </div>
+          <div className="project-tags">
+            {this.renderProjectTags()}
+          </div>
         </div>
         <div className="text-button projects-x"
           onClick={()=>this.setState({ currentProject: "" })}>
           x
         </div>
-
       </div>
     );
   }
